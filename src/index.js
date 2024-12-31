9234ef59396769f15550ba7ec6e42fd3b0f9aad2
Dylan Gray <dylan.gray@revenuepathgroup.com>
2024-12-31 17:23:37 -0500

    no changes made

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// this file is the entry point for the app
+// it renders the App component to the root element
+// in the public/index.html file
+
 const rootElement = document.getElementById('root');
 console.log('Root element:', rootElement);
 
