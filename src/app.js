61f010e9324dbfc690b828b9115a62fe03531205
Dylan Gray <dylan.gray@revenuepathgroup.com>
2024-12-31 19:03:05 -0500

    insatlled tailwind

--- src/app.js
+++ src/app.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 function Header() {
   console.log('Header component rendered');
-  return <header><h1>Welcome to Dylan's Website</h1></header>;
+  return <header className="bg-blue-500 text-white p-4"><h1>Welcome to Dylan's Website</h1></header>;
 }
 
 function MainContent() {
   console.log('MainContent component rendered');
   return (
-    <main>
+    <main className="p-4">
       <p>This is the main content of the website.</p>
     </main>
   );
@@ -16,15 +16,15 @@ function MainContent() {
 
 function Footer() {
   console.log('Footer component rendered');
-  return <footer><p>&copy; 2023 Dylan's Website</p></footer>;
+  return <footer className="bg-gray-800 text-white p-4"><p>&copy; 2023 Dylan's Website</p></footer>;
 }
 
 function App() {
   console.log('App component rendered');
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       <Header />
-      <MainContent />
+      <MainContent className="flex-grow" />
       <Footer />
     </div>
   );
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// filepath: /home/dgray/personal/dylans-website/tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
