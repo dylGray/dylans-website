296ab8b7ebb4c54270ee883902f8c9fe0ff2398f
Dylan Gray <dylan.gray@revenuepathgroup.com>
2025-01-10 16:00:16 -0500

    ui changes

--- src/app.js
+++ src/app.js
@@ -98,24 +98,25 @@ function App() {
     <div className="min-h-screen flex flex-col bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100">
       <Header />
       <main className="flex-grow p-4 flex flex-col items-center">
-        <div className={`w-full max-w-xl ${isMobile ? '' : 'flex items-center'}`}>
+
+        <div className={`w-full max-w-xl ${isMobile ? 'flex flex-col items-center' : 'flex items-center'}`}>
           <img 
             src="/images/dylan-and-jillian.jpg" 
-            className={`rounded-full border-4 border-gray-300 dark:border-gray-700 bg-gray-200 dark:bg-gray-800 w-48 h-48 object-cover ${isMobile ? 'mb-4' : 'mr-4'}`} 
+            className={`rounded-full border-4 border-gray-300 dark:border-gray-700 bg-gray-200 dark:bg-gray-800 w-48 h-48 object-cover ${isMobile ? 'mb-4 mt-8' : 'mr-4'}`} 
           />
           <p 
             id="welcome-text" 
-            className={`text-2xl md:text-3xl font-semibold ${isMobile ? 'mt-3 text-left' : 'mt-0 text-right ml-5'}`}
+            className={`text-2xl md:text-3xl font-semibold ${isMobile ? 'mt-3 text-center' : 'mt-0 text-right'}`}
           ></p>
         </div>
 
         <section style={{ marginTop:"50px" }} className="about-me text-left mb-6 w-full max-w-xl">
           <h2 className="text-xl md:text-2xl mb-4 font-semibold">A little about me...</h2>
           <p className="text-sm md:text-base font-light">
-            I'm currently in my senior year at <strong>Indiana University Bloomington</strong>, pursuing a degree in Informatics. In the informatics program, I'm focusing my studies on <strong>Web Development</strong> and <strong>Ethical Computing</strong>.
+            I'm currently in my senior year at <strong>Indiana University Bloomington</strong>, pursuing a degree in Informatics. In the informatics program, I'm focusing my studies on <strong>Web Development</strong> and <strong>Human-Centered Computing</strong>.
           </p>
           <p className="mt-3 text-sm md:text-base font-light">
-            Post graduation, I will be joining <strong>Revenue Path Group</strong> as a software developer and consultant.
+            Post graduation, I will be joining <strong>Revenue Path Group</strong> as the <strong>Director of Product Development & Engineering</strong>. I'm excited to be working with developers and clients to create innovative solutions that drive business growth.
           </p>
         </section>
         <hr className="my-2 border-t-2 border-gray-300 dark:border-gray-700 w-full max-w-2xl" />
