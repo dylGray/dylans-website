00b630bbaffd22fec3609dfb68f28b9d9ce50b9d
graydc <graydc@iu.edu>
2025-02-13 15:28:04 -0500

    removed import not being used

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BookOpen } from 'lucide-react';
 import { motion, useInView } from "framer-motion";
 import { useRef } from "react";
 import { useState, useEffect } from "react";
